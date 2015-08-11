2e0b80e730a5953f9689961d06d136adbb15f689
sbpolicar <sbpolicar@gmail.com>
2015-08-11 15:09:09 -0700

    added branch for tag feature test

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
 var request = require('request');
+var methodOverride = require('method-override');
 var app = express();
+var ejsLayouts = require('express-ejs-layouts');
 
 require('express-helpers')(app);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
 app.use(ejsLayouts);
+
 app.use('/favorites', require('./controllers/favorites.js'))
 app.use('/favorites/', require('./controllers/comments.js'))
 app.use('/', require('./controllers/movies.js'))
